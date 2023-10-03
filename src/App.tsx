6bebe170b9ef95c30d09921c3117d8e01ee46244
saracrz <sara.crz10@gmail.com>
2023-10-03 20:27:13 +0100

    Add Table component in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-export function App() {
+import { Table } from "./components";
+
+const headers = [
+	{
+		key: "name",
+		label: "Name",
+	},
+	{
+		key: "email",
+		label: "Email",
+	},
+	{
+		key: "phoneNumber",
+		label: "Phone Number",
+	},
+];
+
+const rows = [
+	{
+		name: "Peter",
+		email: "peter@gmail.com",
+		phoneNumber: "00123123123",
+	},
+	{
+		name: "Steven",
+		email: "steven@gmail.com",
+		phoneNumber: "00123123123",
+	},
+];
+
+export const App = () => {
 	return (
 		<div className="App">
-			<h2>Contacts App</h2>
+			<h2>Contacts</h2>
+			<Table headers={headers} rows={rows} />
 		</div>
 	);
-}
+};
