fdcc2457161f1ec270417d078f12351570c03eab
saracrz <sara.crz10@gmail.com>
2023-10-03 20:36:24 +0100

    Update test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -5,7 +5,7 @@ import { App } from "../src/App";
 test("App component display header", () => {
 	render(<App />);
 
-	const heading = screen.getByText("Contacts App");
+	const heading = screen.getByText("Contacts");
 
 	expect(heading).toBeInTheDocument();
 });
