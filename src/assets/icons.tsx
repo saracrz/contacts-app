fbc2891e7cfd434da24d0888980122c636f8b366
saracrz <sara.crz10@gmail.com>
2023-10-10 15:08:39 +0100

    Add sort icon

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "../styles";
 
-export const DeleteIcon = ({ onClick }: { onClick: () => void }) => (
+export const DeleteIcon = ({ onClick }: { onClick?: () => void }) => (
 	<Icon
 		xmlns="http://www.w3.org/2000/svg"
 		className="icon icon-tabler icon-tabler-trash"
@@ -23,7 +23,7 @@ export const DeleteIcon = ({ onClick }: { onClick: () => void }) => (
 	</Icon>
 );
 
-export const EditIcon = ({ onClick }: { onClick: () => void }) => (
+export const EditIcon = ({ onClick }: { onClick?: () => void }) => (
 	<Icon
 		xmlns="http://www.w3.org/2000/svg"
 		className="icon icon-tabler icon-tabler-pencil"
@@ -41,4 +41,24 @@ export const EditIcon = ({ onClick }: { onClick: () => void }) => (
 		<path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path>
 		<path d="M13.5 6.5l4 4"></path>
 	</Icon>
+);
+
+export const SortIcon = ({ onClick }: { onClick?: () => void }) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		className="icon icon-tabler icon-tabler-arrows-sort"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		strokeWidth="2"
+		stroke="currentColor"
+		fill="none"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		onClick={onClick}
+	>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+		<path d="M3 9l4 -4l4 4m-4 -4v14"></path>
+		<path d="M21 15l-4 4l-4 -4m4 4v-14"></path>
+	</svg>
 );
