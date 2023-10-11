9c0a76c35166e8c0f3b37b433fdcecc770f7809e
saracrz <sara.crz10@gmail.com>
2023-10-11 20:58:51 +0100

    Add Info component

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -62,3 +62,24 @@ export const SortIcon = ({ onClick }: { onClick?: () => void }) => (
 		<path d="M21 15l-4 4l-4 -4m4 4v-14"></path>
 	</svg>
 );
+
+export const More = ({ onClick }: { onClick: (id: string) => void }) => (
+	<Icon
+		xmlns="http://www.w3.org/2000/svg"
+		className="icon icon-tabler icon-tabler-dots-vertical"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		strokeWidth="2"
+		stroke="currentColor"
+		fill="none"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		onClick={onClick}
+	>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+		<path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+		<path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+		<path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+	</Icon>
+);
